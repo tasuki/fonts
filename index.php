2214407c1b2e252e7fbcc3f402834ae120e6507e
Vit Brunner <vit.brunner@gmail.com>
2016-04-21 09:23:53 +0200

    Flatten font list, add tags

--- index.php
+++ index.php
@@ -1,69 +1,87 @@
 <?php
 
-$fonts = [
-    'Sans' => [
-        'Titillium Web' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Roboto' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Lato' => ['ext' => false, 'weights' => ['400', '400i', '700']],
-        'Open Sans' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Source Sans Pro' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Ubuntu' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Raleway' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Dosis' => ['ext' => true, 'weights' => ['400', '700']],
-    ],
+$fontList = [
+    'Titillium Web' => '400,400i,700,czech,sans',
+    'Roboto' => '400,400i,700,czech,sans',
+    'Lato' => '400,400i,700,sans',
+    'Open Sans' => '400,400i,700,czech,sans',
+    'Source Sans Pro' => '400,400i,700,czech,sans',
+    'Ubuntu' => '400,400i,700,czech,sans',
+    'Raleway' => '400,400i,700,czech,sans',
+    'Dosis' => '400,700,czech,sans',
 
-    'Serif' => [
-        'PT Serif' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Noto Serif' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Noticia Text' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Alegreya' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Goudy Bookletter 1911' => ['ext' => false, 'weights' => ['400']],
-        'Sorts Mill Goudy' => ['ext' => true, 'weights' => ['400', '400i']],
-        'Cardo' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Poly' => ['ext' => false, 'weights' => ['400', '400i']],
-        'Fjord One' => ['ext' => false, 'weights' => ['400']],
-        'Andada' => ['ext' => true, 'weights' => ['400']],
-        'Merriweather' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Libre Baskerville' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Prociono' => ['ext' => false, 'weights' => ['400']],
-        'Fanwood Text' => ['ext' => false, 'weights' => ['400', '400i']],
-        'Lora' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Domine' => ['ext' => true, 'weights' => ['400', '700']],
-        'Gentium Book Basic' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-    ],
+    'PT Serif' => '400,400i,700,czech,serif',
+    'Noto Serif' => '400,400i,700,czech,serif',
+    'Noticia Text' => '400,400i,700,czech,serif',
+    'Alegreya' => '400,400i,700,czech,serif',
+    'Goudy Bookletter 1911' => '400,serif',
+    'Sorts Mill Goudy' => '400,400i,czech,serif',
+    'Cardo' => '400,400i,700,czech,serif',
+    'Poly' => '400,400i,serif',
+    'Fjord One' => '400,serif',
+    'Andada' => '400,czech,serif',
+    'Merriweather' => '400,400i,700,czech,serif',
+    'Libre Baskerville' => '400,400i,700,czech,serif',
+    'Prociono' => '400,serif',
+    'Fanwood Text' => '400,400i,serif',
+    'Lora' => '400,400i,700,czech,serif',
+    'Domine' => '400,700,czech,serif',
+    'Gentium Book Basic' => '400,400i,700,czech,serif',
 
-    'Slab' => [
-        'Bitter' => ['ext' => false, 'weights' => ['400', '400i', '700']],
-        'Bree Serif' => ['ext' => true, 'weights' => ['400']],
-        'Roboto Slab' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Rokkitt' => ['ext' => false, 'weights' => ['400', '700']],
-        'Arvo' => ['ext' => false, 'weights' => ['400', '400i', '700']],
-        'Patua One' => ['ext' => false, 'weights' => ['400']],
-        'Crete Round' => ['ext' => true, 'weights' => ['400', '400i']],
-        'Bevan' => ['ext' => false, 'weights' => ['400']],
-        'Alfa Slab One' => ['ext' => false, 'weights' => ['400']],
-        'Enriqueta' => ['ext' => true, 'weights' => ['400', '700']],
-        'Copse' => ['ext' => false, 'weights' => ['400']],
-        'Ultra' => ['ext' => false, 'weights' => ['400']],
-    ],
+    'Bitter' => '400,400i,700,slab',
+    'Bree Serif' => '400,czech,slab',
+    'Roboto Slab' => '400,400i,700,czech,slab',
+    'Arvo' => '400,400i,700,slab',
+    'Patua One' => '400,slab',
+    'Crete Round' => '400,400i,czech,slab',
+    'Bevan' => '400,slab',
+    'Alfa Slab One' => '400,slab',
+    'Enriqueta' => '400,700,czech,slab',
+    'Copse' => '400,slab',
+    'Ultra' => '400,slab',
 
-    'Other' => [
-        'Playfair Display' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-        'Abril Fatface' => ['ext' => true, 'weights' => ['400']],
-        'Arapey' => ['ext' => false, 'weights' => ['400', '400i', '700']],
-        'Oswald' => ['ext' => true, 'weights' => ['400', '700']],
-        'Fjalla One' => ['ext' => true, 'weights' => ['400']],
-        'Lobster' => ['ext' => true, 'weights' => ['400']],
-        'Lobster Two' => ['ext' => false, 'weights' => ['400', '400i', '700']],
-        'Passion One' => ['ext' => false, 'weights' => ['400', '700']],
-        'Anton' => ['ext' => true, 'weights' => ['400']],
-        'Acme' => ['ext' => false, 'weights' => ['400']],
-        'Amaranth' => ['ext' => false, 'weights' => ['400', '400i', '700']],
-        'Changa One' => ['ext' => false, 'weights' => ['400', '400i']],
-        'Signika' => ['ext' => true, 'weights' => ['400', '700']],
-        'Aller' => ['ext' => true, 'weights' => ['400', '400i', '700']],
-    ],
+    'Playfair Display' => '400,400i,700,czech,didone',
+    'Abril Fatface' => '400,czech,didone',
+    'Arapey' => '400,400i,700',
+    'Oswald' => '400,700,czech',
+    'Fjalla One' => '400,czech',
+    'Lobster' => '400,czech',
+    'Lobster Two' => '400,400i,700',
+    'Passion One' => '400,700',
+    'Anton' => '400,czech',
+    'Acme' => '400',
+    'Amaranth' => '400,400i,700',
+    'Changa One' => '400,400i',
+    'Signika' => '400,700,czech',
 ];
+
+class Font {
+    public function __construct($name, $tagList)
+    {
+        $this->name = $name;
+        $this->tags = explode(',', $tagList);
+    }
+
+    public function weights()
+    {
+        return implode(',', array_filter($this->tags, function($tag) { return preg_match('/^\d/', $tag); }));
+    }
+
+    public function urlName()
+    {
+        return str_replace($this->name);
+    }
+
+    public function hasTag($tag)
+    {
+        return in_array($tag, $this->tags);
+    }
+}
+
+$fonts = [];
+foreach ($fontList as $name => $tagList) {
+    $fonts[] = new Font($name, $tagList);
+}
 
 ?>
 <html>
@@ -71,13 +89,10 @@ $fonts = [
     <title>Favourite fonts comparison</title>
 <?php
 
-foreach ($fonts as $category) {
-    foreach ($category as $font => $params) {
-        extract($params);
-        ?>
-        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=<?php echo str_replace(' ', '+', $font) ?>:<?php echo implode($weights, ',') ?>&subset=latin,latin-ext">
-        <?php
-    }
+foreach ($fonts as $font) {
+    ?>
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=<?php echo $font->urlName() ?>:<?php echo $font->weights() ?>&subset=latin,latin-ext">
+    <?php
 }
 
 ?>
@@ -96,44 +111,38 @@ foreach ($fonts as $category) {
 <body>
 <?php
 
-foreach ($fonts as $category) {
-    foreach ($category as $font => $params) {
-        extract($params);
-        ?>
-        <div class="container">
-            <div class="font"><a href="https://www.google.com/fonts/specimen/<?php echo str_replace(' ', '+', $font) ?>"><?php echo $font ?></a></div>
-            <div class="box big" style="font-family: '<?php echo $font ?>'">
-                <?php if (in_array('400',  $weights)) echo "Hamburgefonstijvyffi<br>" ?>
-                <?php if ($ext) echo "Hámbůřgěfóňšťíjvý<br>" ?>
-                <?php if (in_array('400i', $weights)) echo "<em>Hamburgefonstijvyffi</em><br>" ?>
-                <?php if (in_array('700',  $weights)) echo "<strong>Hamburgefonstijvyffi</strong><br>" ?>
-            </div>
-
-            <div class="box number" style="font-family: '<?php echo $font ?>'">
-                1234567890<br>
-                1111111111<br>
-                0000000000<br>
-            </div>
-
-            <div class="box small" style="font-family: '<?php echo $font ?>'">
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>
-
-            <div class="box tiny" style="font-family: '<?php echo $font ?>'">
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>
+foreach ($fonts as $font) {
+    ?>
+    <div class="container">
+        <div class="font"><a href="https://www.google.com/fonts/specimen/<?php echo $font->urlName() ?>"><?php echo $font->name ?></a></div>
+        <div class="box big" style="font-family: '<?php echo $font->name ?>'">
+            <?php if ($font->hasTag('400')) echo "Hamburgefonstijvyffi<br>" ?>
+            <?php if ($font->hasTag('czech')) echo "Hámbůřgěfóňšťíjvý<br>" ?>
+            <?php if ($font->hasTag('400i')) echo "<em>Hamburgefonstijvyffi</em><br>" ?>
+            <?php if ($font->hasTag('700')) echo "<strong>Hamburgefonstijvyffi</strong><br>" ?>
         </div>
-        <?php
-    }
 
-    echo '<div class="container"><hr></div>';
+        <div class="box number" style="font-family: '<?php echo $font->name ?>'">
+            1234567890<br>
+            1111111111<br>
+            0000000000<br>
+        </div>
+
+        <div class="box small">
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+
+        <div class="box tiny" style="font-family: '<?php echo $font->name ?>'">
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+    </div>
+    <?php
 }
-
 ?>
 </body>
 </html>
