bc8e6cc1e66a4d98074610e24c74d28831b3b9cc
Vit Brunner <vit.brunner@gmail.com>
2016-04-25 11:10:17 +0200

    Show/hide fonts with pills

---
+++ fonts.js
@@ -0,0 +1,32 @@
+$(function() {
+    var pills = $('.pill');
+    var containers = $('.font-container');
+
+    $('.pill').click(function() {
+        $(this).toggleClass('selected');
+
+        var required = pills.filter(function(i, pill) {
+            return $(pill).hasClass('selected');
+        }).map(function(i, pill) {
+            return $(pill).text();
+        });
+
+        containers.each(function(i, container) {
+            var tags = $(container).attr('data-tags').split(' ');
+
+            var visible = required.map(function(j, requirement) {
+                return tags.indexOf(requirement) != -1;
+            }).toArray().reduce(function(prev, cur) {
+                return prev && cur;
+            }, true);
+
+            if (visible) {
+                $(container).show(400);
+            } else {
+                $(container).hide(400);
+            }
+        });
+
+        return false;
+    });
+});
